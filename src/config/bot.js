fcb0284f872abe8816f7b103407a1d0c43d6b47c
AdenTan123 <debbieng1677@gmail.com>
2026-06-09 17:19:26 +0800

    updated

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "h. jack is cool trust",
+        name: "DM Me you POSTER SUBMISSIONS!",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 4, 
       },
     ],
   },
