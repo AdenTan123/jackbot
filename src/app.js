9f12b86b27631262d48bb4b5c625470028089dcf
Ng Debbie <ngdebbie@Ngs-MacBook-Air.local>
2026-06-12 15:40:34 +0800

    add clear guild commands fixed

--- src/app.js
+++ src/app.js
@@ -277,11 +277,6 @@ process.on('uncaughtException', (err) => {
   logger.error('Uncaught Exception:', err);
 });
 
-// TEMPORARY - remove after one deploy
-import { REST, Routes } from 'discord.js';
-const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
-await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: [] });
-console.log('✅ Guild commands cleared!');
-process.exit(0);
+bot.start();
 
 export default TitanBot;
